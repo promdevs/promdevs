7eeac79088b7911d4a77002168dae4a7199d5afa
Moses <muzardemoses@gmail.com>
2026-03-02 16:57:55 +0100

    Remove unused Discord icon import from HomePage component

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { About } from "@/components/sections/About";
 import { Contact } from "@/components/sections/Contact";
 import { Hero } from "@/components/sections/Hero";
 import { Services } from "@/components/sections/Services";
-import { Github, Linkedin, X, Discord, Youtube } from "@/components/icons";
+import { Github, Linkedin, X, Youtube } from "@/components/icons";
 
 export default function HomePage() {
   return (
